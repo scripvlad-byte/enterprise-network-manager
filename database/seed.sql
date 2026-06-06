b8633649850f0eb826930903a2d6a3e52358d88f
scripvlad-byte <scrip.vlad@gmail.com>
2026-06-06 20:52:02 +0300

    Update seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -1,1 +1,8 @@
+USE enterprise_network;
 
+INSERT INTO devices (hostname, ip_address, status)
+VALUES
+('Router-01', '8.8.8.8', 'Unknown'),
+('Switch-01', '1.1.1.1', 'Unknown'),
+('Server-01', '192.168.1.1', 'Unknown'),
+('Firewall-01', '192.168.0.1', 'Unknown');
