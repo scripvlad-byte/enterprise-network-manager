ddded5e9378fe7373ede4501e213f99fe4dff934
scripvlad-byte <scrip.vlad@gmail.com>
2026-06-06 20:56:24 +0300

    Update devices.py

--- routes/devices.py
+++ routes/devices.py
@@ -1,1 +1,9 @@
+from flask import Blueprint, request, jsonify
+from services.device_service import get_devices
 
+devices_bp = Blueprint("devices", __name__)
+
+@devices_bp.route("/devices", methods=["GET"])
+def devices_list():
+    devices = get_devices()
+    return jsonify(devices)
