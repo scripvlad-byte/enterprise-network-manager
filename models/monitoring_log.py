d433a96b8551a10393c3ccff8aa768f03a5f591e
scripvlad-byte <scrip.vlad@gmail.com>
2026-06-06 20:55:27 +0300

    Update monitoring_log.py

--- models/monitoring_log.py
+++ models/monitoring_log.py
@@ -1,1 +1,6 @@
-
+class MonitoringLog:
+    def __init__(self, id, device_id, status, checked_at):
+        self.id = id
+        self.device_id = device_id
+        self.status = status
+        self.checked_at = checked_at
