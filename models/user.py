921287d1ea268dde06b85535983faf642f094a44
scripvlad-byte <scrip.vlad@gmail.com>
2026-06-06 20:55:19 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,6 @@
-
+class User:
+    def __init__(self, id, username, password, role):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.role = role
