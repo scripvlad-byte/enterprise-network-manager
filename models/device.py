76ae82c79e42de1077b6e1e21efba7c4198a9702
scripvlad-byte <scrip.vlad@gmail.com>
2026-06-06 20:48:54 +0300

    Update device.py

--- models/device.py
+++ models/device.py
@@ -1,1 +1,6 @@
-
+class Device:
+    def __init__(self, id, hostname, ip_address, status):
+        self.id = id
+        self.hostname = hostname
+        self.ip_address = ip_address
+        self.status = status
