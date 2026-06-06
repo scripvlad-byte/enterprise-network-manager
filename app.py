1a82c1f99cb6bfbc32fa2d36a54a4059439d7093
scripvlad-byte <scrip.vlad@gmail.com>
2026-06-06 20:25:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_templates
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def dashboard()
+    return render_templates('dashboard.html')
+
+if __name__ = '__main__'
+app.run(debug = True)
