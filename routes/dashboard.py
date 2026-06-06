05cbb4de8079258e18a2c7aabf7146af80260b43
scripvlad-byte <scrip.vlad@gmail.com>
2026-06-06 20:56:18 +0300

    Update dashboard.py

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template
+from services.device_service import get_devices
+from services.monitoring_service import update_statuses
 
 dashboard_bp = Blueprint("dashboard", __name__)
 
 @dashboard_bp.route("/")
 def dashboard():
-    devices = [
-        {"hostname": "Router", "ip_address": "8.8.8.8", "status": "Online"}
-    ]
+    update_statuses()
+    devices = get_devices()
 
     return render_template("dashboard.html", devices=devices)
