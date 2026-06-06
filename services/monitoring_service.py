22577654e04993b4a40686aa4301c3b4e7213521
scripvlad-byte <scrip.vlad@gmail.com>
2026-06-06 20:48:40 +0300

    Update monitoring_service.py

--- services/monitoring_service.py
+++ services/monitoring_service.py
@@ -1,1 +1,6 @@
+from services.ping_service import check_host
 
+def update_device_status(devices):
+    for device in devices:
+        device["status"] = check_host(device["ip_address"])
+    return devices
